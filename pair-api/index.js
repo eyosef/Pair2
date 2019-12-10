c3bc938a003e7db8019ad3718e973648b893d3d6
Eden <eden.s.mesfun@gmail.com>
2019-12-10 14:01:44 -0500

    figuring out how to seed AWS DB via serverless

--- pair-api/index.js
+++ pair-api/index.js
@@ -4,14 +4,57 @@ const express = require("express");
 const app = express();
 const AWS = require("aws-sdk");
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+// import * as arterraWines from "./db/virginiaWine/arterraWines";
+// const test = require("./db/virginiaWine/arterraWines");
+// import arterraWines from "./db/virginiaWine/arterraWines";
 
 app.use(bodyParser.json({ strict: false }));
 
-app.get("/pair/wine", function(req, res) {
+app.get("/pair/wine", (req, res) => {
   res.send({
     success: "true",
-    message: "Hello World!"
+    message: [
+      {
+        name: "Reserve",
+        wineType: "red",
+        dry: true,
+        sweet: false,
+        flavorProfile: ["full bodied"],
+        imageLink:
+          "https://images.pexels.com/photos/268858/pexels-photo-268858.jpeg?cs=srgb&dl=clean-clear-close-up-268858.jpg&fm=jpg",
+        fruitFlavorProfile: ["black fruit"],
+        winery: "Arterra Wines",
+        city: "Delaplane",
+        state: "VA",
+        tags: [
+          "cabernet sauvignon",
+          "tannat",
+          "petit verdot",
+          "cabernet franc"
+        ],
+        foodPair: []
+      },
+      {
+        name: "Reserve",
+        wineType: "red",
+        dry: true,
+        sweet: false,
+        flavorProfile: ["full bodied"],
+        imageLink:
+          "https://images.pexels.com/photos/268858/pexels-photo-268858.jpeg?cs=srgb&dl=clean-clear-close-up-268858.jpg&fm=jpg",
+        fruitFlavorProfile: ["black fruit"],
+        winery: "Arterra Wines",
+        city: "Delaplane",
+        state: "VA",
+        tags: [
+          "cabernet sauvignon",
+          "tannat",
+          "petit verdot",
+          "cabernet franc"
+        ],
+        foodPair: []
+      }
+    ]
   });
 });
 
