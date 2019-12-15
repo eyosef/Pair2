ba15fb5dff0bf73b875563209336191754b3fbd5
Eden <eden.s.mesfun@gmail.com>
2019-12-15 17:11:09 -0500

    created function that scrapes from 1 website. Updated serverless.yml configuration to include TODOs

--- pair-api/fetch.js
+++ pair-api/fetch.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const site1 = "http://868estatevineyards.orderport.net/wines/New-Releases";
-const site2 = "(https://www.arterrawines.com/wine-store/)";
+const site2 = "https://www.arterrawines.com/wine-store/";
 const site3 = "https://www.bluestonevineyard.com/wine.html";
 
 const fetchData = async siteURL => {
@@ -10,39 +10,32 @@ const fetchData = async siteURL => {
   return cheerio.load(result.data);
 };
 
-const setResults = async () => {
-  const data = await fetchData(site1);
-  const data1 = await fetchData(site2);
-  const data2 = await fetchData(site3);
+const setResults = async scrapeData => {
+  const { website, elements } = scrapeData;
+  const { wrapper, name, description } = elements;
+  const data = await fetchData(website);
+  // const data1 = await fetchData(site2);
+  // const data2 = await fetchData(site3);
 
-  const bottleNames = data(".prod-name")
-    .text()
-    .trim()
-    .split("  ")
-    .filter(name => name.length > 0);
-
-  const test = data(".prod-summary");
-
-  // debugger;
-
-  //   return $(".prod-summary").map(node => {
-  //     debugger;
-
-  //     const postJobButton = $(".top > .action-post-job").text();
-  //   });
-
-  return bottleNames.map(async name => {
-    const bottleName = name.trim();
-
-    const data = await fetchData();
-    const desc = data(".prod-summary div").map;
+  const bottles = data(wrapper).map((index, node) => {
+    const bottleName = data(node)
+      .find(name)
+      .text()
+      .trim();
+    const bottleDescription = data(node)
+      .find(description)
+      .text()
+      .trim();
+    return { name: bottleName, description: bottleDescription };
   });
-  //   bottleNames.trim().split("  ").filter(name => name !== " " || "" )
-  //bottleNames.trim()
-  //   const bottleDescription = data(".prod-content")
-  //     .text()
-  //     .split("  ")
-  //     .filter(name => name.length > 2);
+  return bottles;
 };
 
-setResults();
+setResults({
+  website: site1,
+  elements: {
+    wrapper: ".shop-holder > .data > .productItem",
+    name: ".prod-name",
+    description: ".prod-content"
+  }
+});
